a2b1879b1671677449f9167e4167da937b815c8c
Goose <drsail043@gmail.com>
2019-07-29 23:26:11 +0300

    Thread with lambda
    Example

---
+++ Thread_4/Thread_4/Thread_4.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <thread>
+#include <chrono>
+
+using namespace std;
+
+int Sum(int a, int b) {
+	this_thread::sleep_for(chrono::milliseconds(2000));
+
+	cout << "id thread = " << this_thread::get_id() << " ===========\t" << __FUNCTION__ << " STARTED \t=========" << endl;
+
+	this_thread::sleep_for(chrono::milliseconds(5000));
+
+	cout << "id thread = " << this_thread::get_id() << " =========== \t" << __FUNCTION__ << " STOPPED \t=========" << endl;
+	return a + b;
+}
+
+int main()
+{
+	int result;
+	//cout << "Sum results = " << result << endl;
+
+	thread t([&result]() 
+	{ 
+		result = Sum(2, 5); 
+	});
+
+	for (size_t i = 0; true; i++)
+	{
+		cout << "id thread = " << this_thread::get_id() << "\tmain works\t" << i << endl;
+		this_thread::sleep_for(chrono::milliseconds(500));
+	}
+
+	t.join();
+	cout << result << endl;
+
+
+	return 0;
+}
