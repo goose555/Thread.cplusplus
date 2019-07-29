6a59d6258216f27e92e5e2fb546539a2d979ca44
Goose <drsail043@gmail.com>
2019-07-29 21:28:20 +0300

    Simple thread
    Example

---
+++ Thread_1/Thread_1/Thread_1.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <thread>
+#include <chrono>
+
+using namespace std;
+
+void DoWork() {
+	for (size_t i = 0; i < 10; i++)
+	{
+		this_thread::sleep_for(chrono::milliseconds(250));
+		cout << "Thread id: " << this_thread::get_id() << "\tDoWork\t" << i << endl;
+	}
+}
+
+int main(int argc, char* argv[])
+{
+	setlocale(LC_ALL, "ru");
+
+	/*if (argc > 1)
+		cout << argc << endl;
+	else
+		cout << "Not arguments" << endl;*/
+
+	thread th(DoWork);
+	thread th2(DoWork);
+
+	//th.detach(); //Выполняется самостоятельно, не учитывает другой поток (из main)
+	
+	for (size_t i = 0; i < 10; i++)
+	{
+		this_thread::sleep_for(chrono::milliseconds(100));
+		cout << "Thread id: " << this_thread::get_id() << "\tmain\t" << i << endl;
+	}
+
+	th.join(); //Дождаться выполнения (где хотим дождаться выполнения потока)
+	//Произойдет блокировка основного потока
+	th2.join();
+	return 0;
+}
